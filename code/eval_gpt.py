709e00e804af988d340d48f5bb96cf8442c8729a
chenyuen0103 <chenyuen0103@berkeley.edu>
2023-09-02 19:52:26 -0500

    run django

--- code/eval_gpt.py
+++ code/eval_gpt.py
@@ -481,7 +481,7 @@ if __name__ == '__main__':
     # main(args.file_path, args.dataset)
     model_list = ['text-davinci-002', 'text-davinci-001', 'text-davinci-003','gpt-4-0613']
     # main(f"{out_dir}/gpt-4-0613/requests_direct_newstest.csv","newstest",False)
-    main(f"{out_dir}/gpt-4-0613/requests_amr_django.csv", "django", False)
+    main(f"{out_dir}/gpt-3.5-turbo-0613/requests_amr_django.csv", "django", False)
     # main("/Users/chenyuen/Desktop/amr_llm/data/ablations/text_ablation_1_only.csv", "entity_recog", True)
     # for m in model_list:
     #     for file in os.listdir(out_dir/m):
