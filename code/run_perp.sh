0d56706e9a08fcb235677b35d9b3736a7678d4be
chenyuen0103 <chenyuen0103@berkeley.edu>
2023-09-05 12:34:24 -0500

    run entity_recog_gold on 001

--- code/run_perp.sh
+++ code/run_perp.sh
@@ -3,10 +3,11 @@
 ## Loop through each .txt file in data/txt_files/
 #for data_file in data/txt_files/*.txt; do
 #    # Extract the base name of the file (remove .txt extension)
+#    echo "Processing $data_file"
 #    base_name=$(basename "$data_file" .txt)
 #
 #    # Define the output .json file name
-##    data_file = "data/txt_files/${base_name}.txt"
+#    data_file="data/txt_files/${base_name}.txt"
 #    out_file="data/txt_files/${base_name}.json"
 #
 #    # Run the Python script
