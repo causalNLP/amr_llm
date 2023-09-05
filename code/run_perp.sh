1b7d758eddfe5903a601b5729fb9cbcd609697d3
chenyuen0103 <chenyuen0103@berkeley.edu>
2023-09-04 20:35:48 -0500

    updat run_perp.sh

--- code/run_perp.sh
+++ code/run_perp.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+#
+## Loop through each .txt file in data/txt_files/
+#for data_file in data/txt_files/*.txt; do
+#    # Extract the base name of the file (remove .txt extension)
+#    base_name=$(basename "$data_file" .txt)
+#
+#    # Define the output .json file name
+##    data_file = "data/txt_files/${base_name}.txt"
+#    out_file="data/txt_files/${base_name}.json"
+#
+#    # Run the Python script
+#    python ~/Desktop/CausalLLMs/perplexity_calculator.py -f "$data_file" --out-file "$out_file"
+#done
 
-# Loop through each .txt file in data/txt_files/
-for data_file in data/txt_files/*.txt; do
-    # Extract the base name of the file (remove .txt extension)
+
+#!/bin/bash
+
+# Export the Python script path as a variable
+export SCRIPT_PATH=../CausalLLMs/perplexity_calculator.py
+
+# Define the function to run the script
+run_script() {
+    data_file=$1
     base_name=$(basename "$data_file" .txt)
-
-    # Define the output .json file name
-#    data_file = "data/txt_files/${base_name}.txt"
     out_file="data/txt_files/${base_name}.json"
+    python $SCRIPT_PATH -f "$data_file" --out-file "$out_file"
+}
 
-    # Run the Python script
-    python ~/Desktop/CausalLLMs/perplexity_calculator.py -f "$data_file" --out-file "$out_file"
-done
+# Export the function so GNU Parallel can use it
+export -f run_script
+
+# Run the function in parallel for each .txt file
+find data/txt_files/ -name "*.txt" | parallel -j 4 run_script
